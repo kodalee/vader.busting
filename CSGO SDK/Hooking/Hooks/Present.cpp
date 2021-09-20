738b3029894524546afbd11417408121ba577e8d
xentux0 <freec990@gmail.com>
2021-09-20 19:43:32 -0400

    I fucked exons mom and his dad

--- CSGO SDK/Hooking/Hooks/Present.cpp
+++ CSGO SDK/Hooking/Hooks/Present.cpp
@@ -15,10 +15,6 @@ HRESULT __stdcall Hooked::Present( LPDIRECT3DDEVICE9 pDevice, const RECT* pSourc
 		// gay idc
 		InputHelper::Update( );
 
-		if( InputSys::Get()->WasKeyPressed( g_Vars.menu.key.key ) ) {
-			g_Vars.globals.menuOpen = !g_Vars.globals.menuOpen;
-		}
-
 		Render::DirectX::begin( );
 		{
 			GUI::ctx->animation = g_Vars.globals.menuOpen ? ( GUI::ctx->animation + ( 1.0f / 0.2f ) * Interfaces::m_pGlobalVars->frametime )
