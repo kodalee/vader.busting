acdda3e35d9c1d880e4e86f7908121e46eed60c1
19467 <77497933+19467@users.noreply.github.com>
2022-01-24 11:28:03 -0500

    Update AntiAim.cpp

--- CSGO SDK/Features/Rage/AntiAim.cpp
+++ CSGO SDK/Features/Rage/AntiAim.cpp
@@ -303,9 +303,13 @@ namespace Interfaces
 
 		static bool restoreFakelag = false;
 
-        if (g_Vars.rage.dt_exploits && g_Vars.rage.key_dt.enabled && *bSendPacket) {
+        if (g_Vars.rage.dt_exploits && g_Vars.rage.key_dt.enabled) {
 
-			if (g_Vars.rage.exploit_lag) {
+			if (g_Vars.misc.mind_trick_test && g_Vars.misc.mind_trick_bind.enabled) {
+				g_Vars.globals.shift_amount = Interfaces::m_pGlobalVars->tickcount % 16 ? 16 : 0;
+			}
+
+			else if (g_Vars.rage.exploit_lag) {
 				g_Vars.globals.shift_amount = Interfaces::m_pGlobalVars->tickcount % 16 > 0 ? 16 : 0;
 			}
 
