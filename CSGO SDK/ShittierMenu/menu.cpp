ca4f1139488e559315d614313bb4a2b0c9bc5f88
Ex10902 <acchack122@gmail.com>
2022-03-23 02:02:10 +0200

    Update menu.cpp

--- CSGO SDK/ShittierMenu/menu.cpp
+++ CSGO SDK/ShittierMenu/menu.cpp
@@ -435,7 +435,7 @@ void HvH()
 				biggestMeme2();
 				ImGui::Hotkey(XorStr("##Fake-walk key"), &g_Vars.misc.slow_walk_bind.key, &g_Vars.misc.slow_walk_bind.cond, ImVec2{ 40,20 });
 				if (g_Vars.misc.slow_walk) {
-					InsertSliderInt(XorStr("Fake-walk speed"), &g_Vars.misc.slow_walk_speed, 1, 16, XorStr("%d"));
+					InsertSliderInt(XorStr("Fake-walk speed"), &g_Vars.misc.slow_walk_speed, 2, 16, XorStr("%d"));
 				}
 
 				break;
