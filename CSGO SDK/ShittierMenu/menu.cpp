e884223900f554b10637dde6ae3754f8349de7c8
Ex10902 <acchack122@gmail.com>
2022-04-15 03:32:32 +0300

    Cool fucking loading screen

--- CSGO SDK/ShittierMenu/menu.cpp
+++ CSGO SDK/ShittierMenu/menu.cpp
@@ -1523,8 +1523,89 @@ ImFont* gravity, *gravityBold, *StarWars, *watermark;
 #define MENU_WIDTH 786.f
 #define MENU_HEIGHT 650.f
 
+bool Spinner(const char* label, float radius, int thickness, const ImU32& color) {
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+	if (window->SkipItems)
+		return false;
+
+	ImGuiContext& g = *GImGui;
+	const ImGuiStyle& style = g.Style;
+	const ImGuiID id = window->GetID(label);
+
+	ImVec2 pos = ImVec2(Render::GetScreenSize().x * 0.5f, Render::GetScreenSize().y * 0.5f);
+	ImVec2 size((radius) * 2, (radius + style.FramePadding.y) * 2);
+
+	const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
+	ImGui::ItemSize(bb, style.FramePadding.y);
+	if (!ImGui::ItemAdd(bb, id))
+		return false;
+
+	// Render
+	window->DrawList->PathClear();
+
+	int num_segments = 30;
+	int start = abs(ImSin(g.Time * 1.8f) * (num_segments - 5));
+
+	const float a_min = IM_PI * 2.0f * ((float)start) / (float)num_segments;
+	const float a_max = IM_PI * 2.0f * ((float)num_segments - 3) / (float)num_segments;
+
+	const ImVec2 centre = ImVec2(pos.x + radius, pos.y + radius + style.FramePadding.y);
+
+	for (int i = 0; i < num_segments; i++) {
+		const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
+		window->DrawList->PathLineTo(ImVec2(centre.x + ImCos(a + g.Time * 8) * radius,
+			centre.y + ImSin(a + g.Time * 8) * radius));
+	}
+
+	window->DrawList->PathStroke(color, false, thickness);
+}
+
 void IMGUIMenu::Render()
 {
+
+	static bool load = true;
+	static float flCurTime;
+	static bool init = true;
+
+	if (init) {
+		flCurTime = Interfaces::m_pGlobalVars->curtime;
+		init = false;
+	}
+
+	float flSubtractedTime = Interfaces::m_pGlobalVars->curtime - flCurTime;
+
+	if (flSubtractedTime > 7.f) { // 7 seconds i think is the optimal time
+		load = false;
+	}
+
+	if(load)
+	{
+		static bool retard = true;
+
+		ImGui::SetNextWindowPos(ImVec2(Render::GetScreenSize().x * 0.5f, Render::GetScreenSize().y * 0.5f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+
+		ImGui::SetNextWindowSize(ImVec2(Render::GetScreenSize().x, Render::GetScreenSize().y));
+
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.0f, 0.0f, 0.0f, 0.3f)); // Set window background to black
+
+		ImGui::Begin(XorStr("Loading"), &retard, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysUseWindowPadding | ImGuiWindowFlags_NoMove);
+
+		ImGui::PopStyleColor();
+
+		ImGui::PushFont(watermark);
+
+		ImGui::SameLine(Render::GetScreenSize().x * 0.5f - 55, -1.0f, Render::GetScreenSize().y * 0.5f + 40);
+		ImGui::Text(XorStr("Initializing Vader.tech"));
+
+		ImGui::PopFont();
+
+		Spinner(XorStr("##spinner"), 15, 6, ImColor(255, 215, 0));
+
+		ImGui::End();
+
+		return;
+	}
+
 	if (!Opened) return;
 
 	//ImGui::GetIO().MouseDrawCursor = _visible;
