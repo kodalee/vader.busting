15f9c39798c06a738917cde9b17c232c7b45b0aa
Ex10902 <acchack122@gmail.com>
2022-10-14 10:41:22 +0300

    ghetto asf idc

--- CSGO SDK/Features/Visuals/Hitmarker.cpp
+++ CSGO SDK/Features/Visuals/Hitmarker.cpp
@@ -41,14 +41,12 @@ void Hitmarkers::RenderWorldHitmarkers( ) {
 
 		// If the delta between the current time and hurt time is larger than 0.5 seconds then we should erase
 		if( Interfaces::m_pPrediction->GetUnpredictedGlobals( )->curtime - info.m_flTime > 0.5f ) {
-			if (m_bShouldDrawDamage) {
-				info.m_flAlpha -= (1.0f / 1.0f) * Interfaces::m_pGlobalVars->frametime;
-				info.m_flAlpha = std::clamp<float>(info.m_flAlpha, 0.0f, 1.0f);
-			}
+			info.m_flAlpha -= ( 1.0f / 1.0f ) * Interfaces::m_pGlobalVars->frametime;
+			info.m_flAlpha = std::clamp<float>( info.m_flAlpha, 0.0f, 1.0f );
 		}
 
 		Vector2D vecPos;
-		if( WorldToScreen( Vector( info.m_flPosX, info.m_flPosY, info.m_flPosZ ), vecPos ) && m_bShouldDrawDamage ) {
+		if( WorldToScreen( Vector( info.m_flPosX, info.m_flPosY, info.m_flPosZ ), vecPos ) ) {
 			constexpr int iLineSize{ 8 };
 
 			if( info.m_flAlpha > 0.0f && g_Vars.esp.visualize_hitmarker_world ) {
