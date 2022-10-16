7f76111042e5401e5a7adb1d60dc8df4935b94b0
Ex10902 <acchack122@gmail.com>
2022-10-16 09:12:54 +0300

    Revert "Revert "Reverted this till i fix later""
    This reverts commit 8c934780321786bb7095d62efd43a3a1984375b6.

--- CSGO SDK/Features/Rage/AnimationSystem.cpp
+++ CSGO SDK/Features/Rage/AnimationSystem.cpp
@@ -851,12 +851,9 @@ namespace Engine
 			};
 
 			if (!player->IsTeammate(C_CSPlayer::GetLocalPlayer())) { // plist overrides
-				player_info_t info;
-				Interfaces::m_pEngine->GetPlayerInfo(player->EntIndex(), &info);
-				//if (g_Vars.globals.player_list.override_pitch[info.userId]) {
-				//	player->m_angEyeAngles().x = g_Vars.globals.player_list.override_pitch_slider[info.userId];
-				//}
-				
+				if (g_Vars.globals.player_list.override_pitch[player->EntIndex()]) {
+					player->m_angEyeAngles().x = g_Vars.globals.player_list.override_pitch_slider[player->EntIndex()];
+				}
 			}
 
 			// attempt to resolve the player	
