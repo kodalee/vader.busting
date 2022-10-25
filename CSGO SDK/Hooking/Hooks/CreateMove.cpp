11bef443e9b05c320120c152d32b48f88f15ec66
19467 <77497933+19467@users.noreply.github.com>
2022-10-25 00:24:47 -0400

    Revert "fix"
    This reverts commit 489bdf0f8d2f227534b1ba9d1398a4019b255e6b.

--- CSGO SDK/Hooking/Hooks/CreateMove.cpp
+++ CSGO SDK/Hooking/Hooks/CreateMove.cpp
@@ -424,6 +424,9 @@ namespace Hooked
 
 		g_Vars.globals.CurrentLocalViewAngles = _cmd->viewangles;
 
+
+		g_Vars.globals.m_vecFixedEyePosition = pLocal->GetEyePosition();
+
 		g_Vars.globals.m_flCurtime = Interfaces::m_pGlobalVars->curtime;
 
 		Encrypted_t<CUserCmd> cmd( _cmd );
@@ -480,8 +483,6 @@ namespace Hooked
 		//g_TickbaseController.PreMovement( );
 
 		//Engine::Prediction::Instance()->KeepCommunication(bSendPacket, cmd->command_number);
-
-		g_Vars.globals.m_vecFixedEyePosition = pLocal->GetEyePosition();
 
 		Engine::Prediction::Instance( )->RunGamePrediction( );
 
