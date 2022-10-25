21e9b89646944d093ee5c931df8df6c7cc2c6332
19467 <77497933+19467@users.noreply.github.com>
2022-10-25 00:24:46 -0400

    Revert "possible occlusion fix"
    This reverts commit 7a186bfe189025ad3d66342560a2a0cd6f657954.

--- CSGO SDK/Features/Miscellaneous/Movement.cpp
+++ CSGO SDK/Features/Miscellaneous/Movement.cpp
@@ -475,9 +475,7 @@ namespace Interfaces
 
 		g_BoneSetup.BuildBones( m_movement_data->m_pLocal, BONE_USED_BY_ANYTHING, BoneSetupFlags::None );
 
-
-		//OLD EYEPOS
-		//g_Vars.globals.m_vecFixedEyePosition = m_movement_data->m_pLocal->GetEyePosition( );
+		g_Vars.globals.m_vecFixedEyePosition = m_movement_data->m_pLocal->GetEyePosition( );
 
 		*( float* )( uintptr_t( m_movement_data->m_pLocal ) + Engine::Displacement.DT_BaseAnimating.m_flPoseParameter + 48 ) = PitchPosBackup;
 
